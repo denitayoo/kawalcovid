10d51466bb2a72582ed0f0109e0d3b35a9728156
denitayoo <denitayo2006@gmail.com>
2021-02-06 13:57:19 +0700

    covid

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
           $this->call([
             ProvinsiSeeder::class,
             KotaSeeder::class,
+            KecamatanSeeder::class,
         ]);
     
     }
